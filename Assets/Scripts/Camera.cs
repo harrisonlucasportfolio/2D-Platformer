bbb2ed42a8de16112e6e09340b70e02664a54978
lucas harrison <lucas harrison>
2017-04-12 14:31:10 -0500

    Initial checkin with very basic 2D platformer protype ready.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject player;
+
+
+    private Vector2 velocity;
+    public float smoothXFactor;
+    public float smoothYFactor;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+        //player = GameObject.FindGameObjectWithTag("Player");	
+        offset = transform.position - player.transform.position;
+	}
+	/*
+    void FixedUpdate()
+    {
+        float x = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothXFactor);
+        float y = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothYFactor);
+
+        // Move the camera to new position
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+    */
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Remoting.Services;
+
+public class GroundCheck : MonoBehaviour {
+
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () 
+    {
+        player = gameObject.GetComponentInParent<PlayerController>();
+
+	}
+	
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        print("Enter2d");
+        player.grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        print("Stay2d");
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        print("Exit2d");
+        player.grounded = false;
+    }
+}
